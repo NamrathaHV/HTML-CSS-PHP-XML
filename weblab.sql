d5bf279eddb6290fa3c4c505a2b3fba710d2a7cf
NAMRATHA.H.V <1rn18cs067.namratha.h.v@gmail.com>
2021-08-28 12:18:07 +0530

    first commit

---
+++ weblab.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `student` (
+  `usn` varchar(10),
+  `name` varchar(20),
+  `addr` varchar(20)
+);
+
+INSERT INTO `student` (`usn`, `name`, `addr`) VALUES
+('1RN18CS044', 'Nisha', 'Bengaluru'),
+('1RN18CS030', 'Manoj', 'Mysore'),
+('1RN18CS002', 'Abhishek', 'Bengaluru'),
+('1RN18CS023', 'Dhilip', 'Tumkur'),
+('1RN18CS014', 'Deepika', 'Hassan');
